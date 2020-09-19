fbbfca1c8619343809ae5897fb2b82d249d1839e
ivainsenker <ivainsenker@intuit.com>
2020-09-19 15:49:57 +0300

    splilt and add ranks

--- csv_split.py
+++ csv_split.py
@@ -2,28 +2,38 @@ import csv
 import sys
 from csv import reader, writer
 
-data = list(reader(open("learning_subset_1000ds.csv", "r"), delimiter=","))
-index = 1
-out = writer(open("output" + str(index) + ".csv", "w"), delimiter=",")
 
-current_value = "x"
 if len(sys.argv) > 1:
     file_name = sys.argv[1]
 else:
-    file_name = "learning_subset_1000ds.csv"
+   # file_name = "learning_subset_1000ds.csv"
+     file_name = "learning_all_moves_step1.csv"
+
+index = 1
+out = writer(open("results/output" + str(index) + ".csv", "w"), delimiter=",")
+current_value = "headlines"
+
+index_of_path_column = 3 # notice that for the small file the index is 2
 
 with open(file_name, "r") as csv_file:
-    csv_reader = csv.reader(csv_file,delimiter=',')
+    csv_reader = csv.reader(csv_file, delimiter=',')
     for row in csv_reader:
-        if current_value == "x":
-            current_value = ""
-            continue
-        if current_value == "":
-            current_value = row[2]
-        if row[2] == current_value:
-            out.writerow(row)
+        if current_value == "first":#if this is the first row of the big file(after headlines)
+            current_value = row[index_of_path_column]
+
+        if current_value == "headlines":#if we are reading the headlines
+            headlines = row
+            out.writerow(headlines)
+            current_value = "first"
         else:
-            current_value = row[2]
-            index += 1
-            out = writer(open("results/output" + str(index-1) + ".csv", "w"), delimiter=",")
-            out.writerow(row)
+            if row[index_of_path_column] == current_value:
+                out.writerow(row)
+            else:
+                current_value = row[index_of_path_column]
+                index += 1
+                out = writer(open("results/output" + str(index) + ".csv", "w", newline=''), delimiter=",")
+                out.writerow(headlines)
+                out.writerow(row)
+
+
+

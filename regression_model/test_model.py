1313e237ef2425ce1ffeb5bc776d6789b0addb7d
ozgranit <ozgranit@gmail.com>
2020-09-25 01:58:50 +0300

    added measurments
    added best_predicted_ranking and best_empirically_ranking, still need to be tested

--- regression_model/test_model.py
+++ regression_model/test_model.py
@@ -1,90 +1,101 @@
+import csv
 import os
 import sys
 import pickle
 import numpy as np
 
 import torch
-import torch.autograd as autograd
 
 import matplotlib.pyplot as plt
+from prepare_data import get_train_batch, FEATURE_LIST
 from lnn_utils import load_model_and_loss
-
+from regression_model import LNN
 
 USE_CUDA = torch.cuda.is_available()
-dtype = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
-
-SavedState = namedtuple("SavedState", "state_dict timestep stats")
-print('******* Running on {} *******'.format('CUDA' if USE_CUDA else 'CPU'))
 
 
-def model_predict(model):
-
-	with torch.no_grad():
-		for x, y in get_test_data():
-			x = torch.tensor(x, dtype=torch.float32)
-			y = torch.tensor(y, dtype=torch.float32)
-
-			predictions = model(x.float())
-			loss = criterion(predictions, y)
-			TestLoss.append(loss.item())
-	return np.mean(TestLoss)
-
-
-def lnn_learning(
-		lnn,
-		time_steps,
-		learning_rate=1e-3,
-		batch_size=32,
-):
-	STATS_FILE_NAME = 'statistics.pkl'
-
-	###############
-	# BUILD MODEL #
-	###############
-
-	x_list, y_list = get_train_batch(batch_size=1)  # for length of x vector
+def get_model(pkl_file='N_params.pkl'):
 	if USE_CUDA:
-		N = lnn(in_features=len(x_list[0]), output=1).cuda()
+		model = LNN(in_features=len(FEATURE_LIST), output=1).cuda()
 	else:
-		N = lnn(in_features=len(x_list[0]), output=1)
+		model = LNN(in_features=len(FEATURE_LIST), output=1)
 
-	# initialize weights
-	N.apply(weights_init_uniform_rule)
-
-	################################
-	# Check & load pretrained model and loss
-	# TestLoss[1] is test lost val, TestLoss[0] is test lost time_step
-	# TrainLoss holds the average loss of last 100 time_steps
-	start, TrainLoss, TestLoss = load_model_and_loss(N)
-	last_1000_train_loss = []  # list of length up to 1000, will be lost in saving and loading
-	################################
-
-	# Loss and Optimizer
-	criterion = nn.L1Loss()
-	optimizer = optim.SGD(N.parameters(), lr=learning_rate)
-	# every step_size we update new_lr = old_lr*gamma
-	# NOTICE in loading we do NOT load the last lr used, so adjust lr manually before starting
-	#scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=(time_steps/10), gamma=0.2)
-
-	LOG_EVERY_N_STEPS = 5000
-	CALC_TEST_EVERY_N_STEPS = 500000
-
-	for t in range(start, time_steps):
-		###################
-		# ACTUAL TRAINING #
-		###################
-		x_train, labels = get_train_batch(batch_size=batch_size)
-		# not sure which one will work #1 or #2
-		# 1:
-		x_train = torch.tensor(x_train, dtype=torch.float32)
-		labels = torch.tensor(labels, dtype=torch.float32)
-		# 2:
-		# x_train = Variable(x_train)
-		# labels = Variable(labels)
-
-		# Forward
-		predictions = N(x_train.float())
+	if os.path.isfile(pkl_file):
+		print('Load N parameters ...')
+		model.load_state_dict(torch.load('N_params.pkl'))
+	else:
+		print('Missing file to Load model')
+		exit()
+	return model
 
 
-	plot_loss(TrainLoss, TestLoss)
+def model_predict(model, x):
+	with torch.no_grad():
+		x = torch.tensor(x, dtype=torch.float32)
+		predictions = model(x.float())
+	predictions = predictions.numpy()
+	predictions = [predictions[i][0] for i in range(len(predictions))]
 	return predictions
+
+
+def handle_row(row):
+	# was modified to handle "outputi_ranks.csv", will not work on other files
+	x = row[1:-2]  # remove label, ranks and first column which aren't attributes
+	rank = row[-1]  # take only rank
+	# replace '' with '0'
+	x = ['0' if a == '' else a for a in x]
+	# we want to return numbers not strings, small nums so float64
+	x = np.array(x, dtype=np.float64)
+	rank = int(rank)
+	return x, rank
+
+
+def get_x_and_ranks(file_name):
+	# matches format of outputi_ranks as taken from the clean 'learning_all_moves_step1'
+	x_list = []  # attribute vectors
+	r_list = []  # correct ranking values
+	with open(file_name, newline='') as f:
+		reader = csv.reader(f)
+		next(reader)  # skip first line, meaning headlines
+		for row in reader:
+			x, r = handle_row(row)
+			x_list.append(x)
+			r_list.append(r)
+	return np.vstack(x_list), r_list
+
+
+# returns the true ranking of the tree that was predicted 1st by the model.
+# assumes predictions[i] matches with rankings[i]
+def best_predicted_ranking(predictions, rankings):
+	argmax = np.argmax(predictions)
+	return rankings[argmax]
+
+
+def test_best_predicted_ranking():
+	a = [40, 20, 3.2, 0, -17]
+	b = [1, 2, 3, 4, 5]
+	res = best_predicted_ranking(a, b)
+	assert (res == 1)
+
+
+#  returns the ranking the model predicted for the (true) best tree
+def best_empirically_ranking(predictions, rankings):
+	bestidx = np.argmin(rankings)
+	order = np.argsort(predictions)
+	return order[bestidx]+1  # to start ranking from 1 not 0
+
+
+def handle_file(model, filename):
+	x, ranks = get_x_and_ranks(filename)
+	predictions = model_predict(model, x)
+	res1 = best_predicted_ranking(predictions, ranks)
+	res2 = best_empirically_ranking(predictions, ranks)
+	return res1, res2
+
+
+if __name__ == '__main__':
+	filename = r"dirpath\results\output29_ranks.csv"
+	N = get_model()
+	res = handle_file(N, filename)
+	print("done")
+	print(res)
